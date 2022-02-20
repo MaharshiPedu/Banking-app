f8e7b1fdcebd673c8a7529d48e1486a243c87233
MaharshiPedu <84611938+MaharshiPedu@users.noreply.github.com>
2022-02-20 14:42:25 +0530

    Add files via upload
    Banking system

---
+++ banking/index.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+if(!isset($_SESSION['userId'])){ header('location:login.php');}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Banking</title>
+  <?php require 'assets/autoloader.php'; ?>
+  <?php require 'assets/db.php'; ?>
+  <?php require 'assets/function.php'; ?>
+  
+</head>
+<body style="background:#3446a8;background-size: 100%">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+ <a class="navbar-brand" href="#">
+    <img src="images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+   <!--  <i class="d-inline-block  fa fa-building fa-fw"></i> --><?php echo bankName; ?>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">  <a class="nav-link" href="accounts.php">Accounts</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="statements.php">Account Statements</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="transfer.php">Funds Transfer</a></li>
+      <!-- <li class="nav-item ">  <a class="nav-link" href="profile.php">Profile</a></li> -->
+
+
+    </ul>
+    <?php include 'sideButton.php'; ?>
+    
+  </div>
+</nav><br><br><br>
+<div class="row w-100" >
+  <div class="col" style="padding: 22px;padding-top: 0">
+    <div class="jumbotron shadowBlack" style="padding: 25px;min-height: 30px;max-height: 30px">
+  <h4 class="display-5">Welcome to MY Bank</h4><br><br><br>
+
+  
+
+<div class="col">
+    <div class="row" style="padding: 22px;padding-top: 0">
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="images/acount.jpg" style="max-height: 155px;min-height: 155px" alt="Card image cap">
+          <div class="card-body">
+            <a href="accounts.php" class="btn btn-outline-success btn-block">Account Summary</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="images/transfer.png" alt="Card image cap" style="max-height: 155px;min-height: 155px">
+        <div class="card-body">
+          <a href="transfer.php" class="btn btn-outline-success btn-block">Transfer Money</a>
+         </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" style="padding: 22px">
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="images/bell.png" style="max-height: 155px;min-height: 155px" alt="Card image cap">
+          <div class="card-body">
+            <a href="notice.php" class="btn btn-outline-primary btn-block">Check Notification</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="images/contacts.png" alt="Card image cap" style="max-height: 155px;min-height: 155px">
+        <div class="card-body">
+          <a href="feedback.php" class="btn btn-outline-primary btn-block">Contact Us</a>
+         </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
